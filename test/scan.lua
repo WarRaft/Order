510377313802bc4f733203b0ad9cefff7a4aa7a2
nazarpunk <nazarpunk@gmail.com>
2023-06-19 18:47:27 +0300

    scan

--- test/scan.lua
+++ test/scan.lua
@@ -1,8 +1,12 @@
-local t = {'aa', 'holybolt', 'dd', 'earthquake', 'a1', '11'}
+local list = { 'aa', 'holybolt', 'dd', 'earthquake', 'a1', '11' }
+local s = '';
 
-for _, v in pairs(t) do
-    if OrderId(v) ~= nill then
+for _, v in pairs(list) do
+    if OrderId(v) > 0 then
         print(v);
-        printc(v);
+        s = s + v + ',';
     end
 end
+
+Preload(s);
+PreloadGenEnd("_test\\order.txt");
