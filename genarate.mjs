02cd8a421aaf54441d9a38f4a36a8d77034a192b
nazarpunk <nazarpunk@gmail.com>
2023-06-19 16:38:21 +0300

    order name

--- genarate.mjs
+++ genarate.mjs
@@ -3,6 +3,7 @@ import {ordersHidden, ordersNamed} from "./orders.mjs";
 
 const vjass = 'OrderId.j';
 const zinc = 'OrderId.zn';
+const zincOrderId2Name = 'OrderId2Name.zn';
 const lua = 'OrderId.lua';
 
 const hiddenHeader = 'Hidden orders';
@@ -10,6 +11,7 @@ const namedHeader = 'Named orders';
 
 fs.writeFileSync(vjass, `globals\n\n\t// ${hiddenHeader}\n`, {flag: 'w+'});
 fs.writeFileSync(zinc, `library OrderId {\n\tpublic {\n\n\t\t// ${hiddenHeader}\n`, {flag: 'w+'});
+fs.writeFileSync(zincOrderId2Name, `library OrderId2Name {\n\tpublic function OrderId2Name(integer id) -> string {\n`, {flag: 'w+'});
 fs.writeFileSync(lua, `-- ${hiddenHeader}\n`, {flag: 'w+'});
 
 /** @param {Object.<string, number>} orders */
@@ -18,6 +20,7 @@ const f = orders => {
         const hex = v.toString(16);
         fs.writeFileSync(vjass, `\tconstant integer Order_${k} = $${hex}\n`, {flag: 'a+'});
         fs.writeFileSync(zinc, `\t\tconstant integer Order_${k} = ${v};\n`, {flag: 'a+'});
+        fs.writeFileSync(zincOrderId2Name, `\t\tif (id == ${v}) return "${k}";\n`, {flag: 'a+'});
         fs.writeFileSync(lua, `Order_${k} = 0x${hex};\n`, {flag: 'a+'});
     }
 }
@@ -33,3 +36,4 @@ f(ordersNamed);
 
 fs.writeFileSync(vjass, `endglobals`, {flag: 'a+'});
 fs.writeFileSync(zinc, `\t}\n}`, {flag: 'a+'});
+fs.writeFileSync(zincOrderId2Name, `\t\treturn I2S(id);\n\t}\n}`, {flag: 'a+'});
