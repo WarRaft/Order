407c7158f78f896258eca1f845ac147c24ac910c
nazarpunk <nazarpunk@gmail.com>
2023-06-19 19:13:02 +0300

    scan update

--- test/scan.lua
+++ test/scan.lua
@@ -1,12 +1,33 @@
-local list = { 'aa', 'holybolt', 'dd', 'earthquake', 'a1', '11' }
+printc('--- start')
+
 local s = '';
 
-for _, v in pairs(list) do
-    if OrderId(v) > 0 then
-        print(v);
-        s = s + v + ',';
+function generateStrings(prefix, length)
+    if length == 0 then
+        if OrderId(prefix) > 0 then
+            printc(prefix);
+            s = s .. prefix .. ',';
+        end
+        return
+    end
+    do
+        local charCode = 48
+        while charCode <= 122 do
+            if charCode >= 48 and charCode <= 57 or charCode >= 97 and charCode <= 122 then
+                local character = string.char(charCode)
+                generateStrings(
+                        nil,
+                        tostring(prefix) .. character,
+                        length - 1
+                )
+            end
+            charCode = charCode + 1
+        end
     end
 end
+generateStrings("", 31)
 
 Preload(s);
-PreloadGenEnd("_test\\order.txt");
+PreloadGenEnd("_test\\\\order.txt");
+
+printc('--- end')
